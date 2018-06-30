8759127c6befb04b34ae929715a38f52c6bf656a
Xiao Xiao <xx2004xiamen@gmail.com>
2018-07-01 02:12:54 +0900

    Fix typo

---
+++ OKKLineSwift-macOS/OKKLineSwift_macOS.h
@@ -0,0 +1,18 @@
+//
+//  OKKLineSwift_macOS.h
+//  OKKLineSwift-macOS
+//
+//  Created by Xiao Xiao on 平成30-06-26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OKKLineSwift_macOS.
+FOUNDATION_EXPORT double OKKLineSwift_macOSVersionNumber;
+
+//! Project version string for OKKLineSwift_macOS.
+FOUNDATION_EXPORT const unsigned char OKKLineSwift_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OKKLineSwift_macOS/PublicHeader.h>
+
+
